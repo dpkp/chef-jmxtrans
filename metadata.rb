0085efb5d14dc7d71532b7bc40b788b423755514
Brian Flad <bflad417@gmail.com>
2014-04-07 20:52:20 -0400

    Rubocop automatic fixes
    Conflicts:
    	attributes/default.rb
    	metadata.rb
    	recipes/default.rb

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,15 @@
-name             "jmxtrans"
-maintainer       "Dana Powers"
-maintainer_email "dana.powers@gmail.com"
-license          "Apache 2.0"
-description      "Installs/Configures jmxtrans"
+name 'jmxtrans'
+maintainer 'Brian Flad'
+maintainer_email 'bflad417@gmail.com'
+license 'Apache 2.0'
+description 'Installs/Configures jmxtrans'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.1"
+version '0.6.0'
 
-%w{ ark }.each do |cb|
+%w(ark).each do |cb|
   depends cb
 end
 
-%w{ centos redhat ubuntu }.each do |os|
+%w(centos redhat ubuntu).each do |os|
   supports os
 end
